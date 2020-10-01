9a4b4887afc2551fcbe9b02c5b5eb40590ac2475
Victor Tiburcio <victor.tiburcio@tecsinapse.com.br>
2020-09-30 20:59:41 -0400

    [FINISHED] Sign In
    - Pubspec update.
    - Readme update.
    - Home Screen update.
    - Mobile Router update.
    - New Sign In Screen.

--- lib/screens/logged.dart
+++ lib/screens/logged.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../controllers/account_controller.dart';
 
 class Logged extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Logged')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Logged'),
+          ),
+          RaisedButton(
+            child: Text('Logout'),
+            onPressed: () {
+              Provider.of<AccountController>(context, listen: false).signOut();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
